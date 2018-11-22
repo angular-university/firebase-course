77670cc330b93343f2e1f4831e87017f3457306c
JHades <jhades.dev@gmail.com>
2018-11-22 15:11:53 +0100

    Angular Firebase Course

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,6 @@
 
 
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Course} from "../model/course";
 import {map} from "rxjs/operators";
@@ -11,7 +10,7 @@ import {Lesson} from "../model/lesson";
 @Injectable()
 export class CoursesService {
 
-    constructor(private http:HttpClient) {
+    constructor() {
 
     }
 
