885906fa47a54c4c76f12db72af9cae9fc3cc620
JHades <jhades.dev@gmail.com>
2018-11-22 13:57:17 +0100

    Angular Firebase Course

---
+++ init-db.ts
@@ -0,0 +1,75 @@
+import {COURSES} from './db-data';
+
+const firebase = require('firebase');
+
+require('firebase/firestore');
+
+var config = {
+  apiKey: 'AIzaSyDAdoefqX5OqjkD3BkW25ZAL6XYZMo4Vz8',
+  authDomain: 'fir-course-17549.firebaseapp.com',
+  databaseURL: 'https://fir-course-17549.firebaseio.com',
+  projectId: 'fir-course-17549',
+  storageBucket: 'fir-course-17549.appspot.com',
+  messagingSenderId: '170806523820'
+};
+
+
+console.log("Uploading data to the database with the following config:\n");
+console.log(JSON.stringify(config));
+
+console.log("\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n");
+
+
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+
+const settings = {timestampsInSnapshots: true};
+
+db.settings(settings);
+
+
+
+
+async function uploadData() {
+
+  var batch = db.batch();
+
+  const courses = db.collection('courses');
+
+
+  Object.values(COURSES)
+    .forEach(course => {
+
+      const newCourse:any = {...course};
+
+      // use Firestore Ids instead
+      delete newCourse.id;
+
+      courses.add(newCourse);
+
+    });
+
+  return batch.commit();
+}
+
+
+
+uploadData()
+  .then(() => {
+    console.log("Writing data, exiting in 5 seconds ...\n\n");
+
+    setTimeout(() => {
+
+      console.log("\n\n\nData Upload Completed.\n\n\n");
+      process.exit(0);
+
+    }, 5000);
+
+  })
+  .catch(err => {
+    console.log("Data upload failed, reason:", err, '\n\n\n');
+    process.exit(-1);
+  });
+
+
