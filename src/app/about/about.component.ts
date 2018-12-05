699d94bf6346ec5d499c2c541b0b91d5dfaf8e56
Your Name <you@example.com>
2018-12-05 16:25:54 +0100

    Serverless Angular with Firebase Course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,23 +4,6 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import {Course} from '../model/course';
 
-const config = {
-    apiKey: "AIzaSyDg8hs704siPZ_chdtodWYpXicMHxz5nhQ",
-    authDomain: "fir-course-recording.firebaseapp.com",
-    databaseURL: "https://fir-course-recording.firebaseio.com",
-    projectId: "fir-course-recording",
-    storageBucket: "fir-course-recording.appspot.com",
-    messagingSenderId: "706691571098"
-};
-
-firebase.initializeApp(config);
-
-const db = firebase.firestore();
-
-const settings = {timestampsInSnapshots:true};
-
-db.settings(settings);
-
 
 
 @Component({
@@ -36,21 +19,6 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-      db.collection('courses').get()
-          .then(snaps => {
-
-
-              const courses : Course[] = snaps.docs.map(snap => {
-
-                  return <Course>{
-                    id: snap.id,
-                    ...snap.data()
-                  }
-                  });
-
-              console.log(courses);
-
-          });
 
   }
 
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Course} from '../model/course';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor() { }
+
+
+
+}
