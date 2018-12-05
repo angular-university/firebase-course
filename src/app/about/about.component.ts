4f90b351146116386728025b5db211f0bdbb9227
Your Name <you@example.com>
2018-12-05 13:58:41 +0100

    Serverless Angular with Firebase Course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,28 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import {Course} from '../model/course';
+
+const config = {
+    apiKey: "AIzaSyDg8hs704siPZ_chdtodWYpXicMHxz5nhQ",
+    authDomain: "fir-course-recording.firebaseapp.com",
+    databaseURL: "https://fir-course-recording.firebaseio.com",
+    projectId: "fir-course-recording",
+    storageBucket: "fir-course-recording.appspot.com",
+    messagingSenderId: "706691571098"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+const settings = {timestampsInSnapshots:true};
+
+db.settings(settings);
+
+
+
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
@@ -7,9 +30,44 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+
+      db.collection('courses').get()
+          .then(snaps => {
+
+
+              const courses : Course[] = snaps.docs.map(snap => {
+
+                  return <Course>{
+                    id: snap.id,
+                    ...snap.data()
+                  }
+                  });
+
+              console.log(courses);
+
+          });
+
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
