65f6fb32c4017dd68328d15c6de524d8d22af286
Your Name <you@example.com>
2019-01-18 15:44:36 +0100

    Serverless Angular with Firebase Course

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as firebaseui from 'firebaseui';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    ui: firebaseui.auth.AuthUI;
 
-  ngOnInit() {
-  }
+    constructor(private afAuth: AngularFireAuth) {
 
+    }
+
+    ngOnInit() {
+
+        const uiConfig = {
+
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ],
+            callbacks: {
+
+                signInSuccessWithAuthResult: this
+                    .onLoginSuccessful
+                    .bind(this)
+            }
+
+        };
+
+        this.ui = new firebaseui.auth.AuthUI(this.afAuth.auth);
+
+        this.ui.start('#firebaseui-auth-container', uiConfig);
+
+
+    }
+
+    onLoginSuccessful() {
+
+
+    }
 }
+
+
