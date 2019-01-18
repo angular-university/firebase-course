f5a54459b7899fea861ec64625d418cbdbf4bd04
Your Name <you@example.com>
2019-01-18 15:45:00 +0100

    Serverless Angular with Firebase Course

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebaseui from 'firebaseui';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 
 
 @Component({
