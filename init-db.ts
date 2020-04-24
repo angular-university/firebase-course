ddfb618c4bfdfc83b244c14cea85bc88ba246739
JHades <jhades.dev@gmail.com>
2020-04-24 11:55:06 +0200

    upgrade to Angular 9

--- init-db.ts
+++ init-db.ts
@@ -4,12 +4,13 @@ import {COURSES, findLessonsForCourse} from './db-data';
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyDNPCbN20dZrAbGChRjyBSDmu2uRiPyBMk",
-    authDomain: "test-a6f61.firebaseapp.com",
-    databaseURL: "https://test-a6f61.firebaseio.com",
-    projectId: "test-a6f61",
-    storageBucket: "test-a6f61.appspot.com",
-    messagingSenderId: "24603706805"
+  apiKey: "AIzaSyDAdoefqX5OqjkD3BkW25ZAL6XYZMo4Vz8",
+  authDomain: "fir-course-17549.firebaseapp.com",
+  databaseURL: "https://fir-course-17549.firebaseio.com",
+  projectId: "fir-course-17549",
+  storageBucket: "fir-course-17549.appspot.com",
+  messagingSenderId: "170806523820",
+  appId: "1:170806523820:web:a3181632d54d076a0bec09"
 };
 
 console.log("Uploading data to the database with the following config:\n");
@@ -18,70 +19,40 @@ console.log(JSON.stringify(config));
 
 console.log("\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n");
 
-firebase.initializeApp(config);
-
+const app = firebase.initializeApp(config);
 const db = firebase.firestore();
 
+main().then(r => console.log('Done.'));
+
 async function uploadData() {
-
-  var batch = db.batch();
-
-  const courses = db.collection('courses');
-
-
-  Object.values(COURSES)
-    .sort((c1:any, c2:any) => c1.seqNo - c2.seqNo)
-    .forEach(async (course:any) => {
-
-      const newCourse = removeId(course);
-
-      const courseRef = await courses.add(newCourse);
-
-      const lessons = courseRef.collection("lessons");
-
-      const courseLessons = findLessonsForCourse(course.id);
-
-      //console.log(`Adding ${courseLessons.length} lessons to ${course.description}`);
-
-      courseLessons.forEach(async lesson => {
-
-        const newLesson = removeId(lesson);
-
-        await lessons.add(newLesson);
-
-      });
-
-    });
-
-  return batch.commit();
+  const courses = await db.collection('courses');
+  for (let course of Object.values(COURSES)) {
+    const newCourse = removeId(course);
+    const courseRef = await courses.add(newCourse);
+    const lessons = await courseRef.collection('lessons');
+    const courseLessons = findLessonsForCourse(course['id']);
+    console.log(`Uploading course ${course['titles']["description"]}`);
+    for (const lesson of courseLessons) {
+      const newLesson = removeId(lesson);
+      await lessons.add(newLesson);
+    }
+  }
 }
 
-
-function removeId(data:any) {
-
+function removeId(data: any) {
   const newData: any = {...data};
-
   delete newData.id;
-
   return newData;
 }
 
-
-uploadData()
-  .then(() => {
-    console.log("Writing data, exiting in 10 seconds ...\n\n");
-
-    setTimeout(() => {
-
-      console.log("\n\n\nData Upload Completed.\n\n\n");
-      process.exit(0);
-
-    }, 10000);
-
-  })
-  .catch(err => {
-    console.log("Data upload failed, reason:", err, '\n\n\n');
-    process.exit(-1);
-  });
-
+async function main(){
+  try {
+    console.log('Start main...\n\n');
+    await uploadData();
+    console.log('\n\nClosing Application...');
+    await app.delete();
+  }catch (e) {
+    console.log('Data upload failed, reason:', e, '\n\n');
+  }
+}
 
