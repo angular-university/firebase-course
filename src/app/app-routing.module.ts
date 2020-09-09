da18c5433d65e94b3b101614da13815aa06d1bae
Sudhir K Bansa <sudhir@techiegeex.com>
2020-09-10 00:20:15 +0530

    Update app-routing.module.ts
    indentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,41 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {CourseComponent} from "./course/course.component";
-import {CourseResolver} from "./services/course.resolver";
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {CourseComponent} from './course/course.component';
+import {CourseResolver} from './services/course.resolver';
 import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
+  {
+    path: '',
+    component: HomeComponent
 
-    },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-      path: "login",
-      component: LoginComponent
-    },
-    {
-        path: 'courses/:courseUrl',
-        component: CourseComponent,
-        resolve: {
-            course: CourseResolver
-        }
-    },
-    {
-        path: "**",
-        redirectTo: '/'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'courses/:courseUrl',
+    component: CourseComponent,
+    resolve: {
+      course: CourseResolver
     }
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
