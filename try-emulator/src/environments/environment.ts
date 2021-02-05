c5bc2a0e6b0425626ce20a4433b0edb0b8c768d1
JHades <jhades.dev@gmail.com>
2021-02-05 14:36:33 +0100

    Firebase & Firestore In Depth course

--- try-emulator/src/environments/environment.ts
+++ try-emulator/src/environments/environment.ts
@@ -6,8 +6,12 @@ export const environment = {
   production: false,
   useEmulators: true,
   firebase: {
-    projectId: 'fir-course-v2',
-    apiKey: 'AIzaSyAsrcKEDOFUC9fKMxxng0Fi6PjqNVQj8vk'
+    apiKey: "AIzaSyAsrcKEDOFUC9fKMxxng0Fi6PjqNVQj8vk",
+    authDomain: "fir-course-v2.firebaseapp.com",
+    projectId: "fir-course-v2",
+    storageBucket: "fir-course-v2.appspot.com",
+    messagingSenderId: "507380388733",
+    appId: "1:507380388733:web:1f20583d30ea71e02cb214"
   }
 };
 
