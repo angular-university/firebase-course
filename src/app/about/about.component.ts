db56d67baa7f81c76ab37348a5c04d06af844dba
Your Name <you@example.com>
2021-03-05 11:15:05 +0100

    Firebase & AngularFire In Depth

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,37 +7,38 @@ import {AngularFirestore} from '@angular/fire/firestore';
 import {COURSES, findLessonsForCourse} from './db-data';
 
 
-
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
-export class AboutComponent  {
+export class AboutComponent {
 
-  constructor(private db: AngularFirestore) {}
-
-  async uploadData() {
-    const coursesCollection = this.db.collection('courses');
-    const courses = await this.db.collection('courses').get();
-    for (let course of Object.values(COURSES)) {
-      const newCourse = this.removeId(course);
-      const courseRef = await coursesCollection.add(newCourse);
-      const lessons = await courseRef.collection('lessons');
-      const courseLessons = findLessonsForCourse(course['id']);
-      console.log(`Uploading course ${course['description']}`);
-      for (const lesson of courseLessons) {
-        const newLesson = this.removeId(lesson);
-        await lessons.add(newLesson);
-      }
+    constructor(private db: AngularFirestore) {
     }
-  }
 
-  removeId(data: any) {
-    const newData: any = {...data};
-    delete newData.id;
-    return newData;
-  }
+    async uploadData() {
+        const coursesCollection = this.db.collection('courses');
+        const courses = await this.db.collection('courses').get();
+        for (let course of Object.values(COURSES)) {
+            const newCourse = this.removeId(course);
+            const courseRef = await coursesCollection.add(newCourse);
+            const lessons = await courseRef.collection('lessons');
+            const courseLessons = findLessonsForCourse(course['id']);
+            console.log(`Uploading course ${course['description']}`);
+            for (const lesson of courseLessons) {
+                const newLesson = this.removeId(lesson);
+                delete newLesson.courseId;
+                await lessons.add(newLesson);
+            }
+        }
+    }
+
+    removeId(data: any) {
+        const newData: any = {...data};
+        delete newData.id;
+        return newData;
+    }
 
 
 }
