38e4b4d5b560651afdfc2e5d7bfafa0fda0f826f
Your Name <you@example.com>
2021-04-23 12:52:11 +0200

    Firebase & AngularFire In Depth

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -13,7 +13,28 @@ import {Observable} from 'rxjs';
 })
 export class EditCourseDialogComponent {
 
-    constructor() {
+    form:FormGroup;
+
+    course: Course;
+
+    constructor(
+        private dialogRef: MatDialogRef<EditCourseDialogComponent>,
+        private fb: FormBuilder,
+        @Inject(MAT_DIALOG_DATA) course: Course,
+    ) {
+
+        this.course = course;
+
+        this.form = this.fb.group({
+            description: [course.description, Validators.required],
+            longDescription: [course.longDescription, Validators.required],
+            promo: [course.promo]
+        });
+
+    }
+
+    close() {
+        this.dialogRef.close();
 
     }
 }
