7a86138dfcff6b03df80064129bd767e81ec0c88
Your Name <you@example.com>
2021-05-10 18:21:33 +0200

    Firebase & AngularFire In Depth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,16 +14,49 @@ import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
+    ui: firebaseui.auth.AuthUI;
 
-    constructor() {
+
+    constructor(
+        private afAuth: AngularFireAuth,
+        private router: Router) {
 
     }
 
     ngOnInit() {
 
+        this.afAuth.app.then(app => {
+            const uiConfig = {
+                signInOptions: [
+                    EmailAuthProvider.PROVIDER_ID,
+                    GoogleAuthProvider.PROVIDER_ID
+                ],
+                callbacks: {
+                    signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this)
+                }
+            };
+
+            this.ui = new firebaseui.auth.AuthUI(app.auth());
+
+            this.ui.start("#firebaseui-auth-container", uiConfig);
+
+            this.ui.disableAutoSignIn();
+        });
+
+
     }
 
     ngOnDestroy() {
+        this.ui.delete();
+    }
+
+    onLoginSuccessful(result) {
+
+        console.log('Firebase UI result:', result);
+
+        this.router.navigateByUrl("/courses");
+
+
 
     }
 }
