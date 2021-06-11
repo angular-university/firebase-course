fda4b236a96bd07fe850b6a032940826d1b593d3
Your Name <you@example.com>
2021-06-11 15:19:50 +0200

    Firebase & AngularFire In Depth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+//
+
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
