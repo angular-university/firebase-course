85914b782607fc018ff16e6c155500cc88d011be
Your Name <you@example.com>
2021-06-15 17:35:04 +0200

    Firebase & AngularFire In Depth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,17 @@ import * as functions from "firebase-functions";
 //
 
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-});
+export const onAddCourseUpdatePromoCounter =
+    functions
+        .runWith({
+            timeoutSeconds: 300,
+            memory: "128MB"
+        })
+        .firestore.document("courses/{courseId}")
+        .onCreate(async (snap, context) => {
+
+            functions.logger.debug(
+                `Running add course trigger for courseId ${context.params.courseId}`);
+
+
+        });
