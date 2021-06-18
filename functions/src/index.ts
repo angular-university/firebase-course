0a381eac64d5bd1ee380dddb12a2faf6b6d26bee
Your Name <you@example.com>
2021-06-18 16:19:46 +0200

    Firebase & AngularFire In Depth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,9 @@
 import * as functions from "firebase-functions";
 
-
-
-
-
 //
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 //
-
 
 export const onAddCourseUpdatePromoCounter =
     functions
@@ -18,10 +13,17 @@ export const onAddCourseUpdatePromoCounter =
         })
         .firestore.document("courses/{courseId}")
         .onCreate(async(snap, context) => {
-
             await (
                 await import("./promotions-counter/on-add-course"))
                 .default(snap, context);
-
         });
 
+
+export const onCourseUpdatedUpdatePromoCounter =
+    functions.firestore
+        .document('courses/{courseId}')
+        .onUpdate(async (change, context) => {
+            await (await import('./promotions-counter/on-course-updated'))
+                .default(change, context);
+
+        })
