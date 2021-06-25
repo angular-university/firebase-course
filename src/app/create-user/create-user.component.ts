a77c8c85ddcba4aaa4a17c75cb509e52c0fc4bcb
Your Name <you@example.com>
2021-06-25 14:39:50 +0200

    Firebase & AngularFire In Depth

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -5,14 +5,30 @@ import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 
 @Component({
-  selector: 'create-user',
-  templateUrl: 'create-user.component.html',
-  styleUrls: ['create-user.component.css']
+    selector: 'create-user',
+    templateUrl: 'create-user.component.html',
+    styleUrls: ['create-user.component.css']
 })
 export class CreateUserComponent {
 
+    form = this.fb.group({
+        email: ['', [Validators.email, Validators.required]],
+        password: ['', [Validators.required, Validators.minLength(5)]],
+        admin: [false]
+    });
 
-  constructor() {
-  }
+    constructor(
+        private fb: FormBuilder,
+        private http: HttpClient) {
+
+    }
+
+    onCreateUser() {
+
+        const user = this.form.value;
+
+        console.log(user);
+
+    }
 
 }
